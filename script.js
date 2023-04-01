3a6e7532d5674f01512e8bba05aca246a0eaca8a
yuliia-zheliakova <121395063+yuliia-zheliakova@users.noreply.github.com>
2023-04-01 14:33:24 +0200

    Added Bootstrap
    - Redesigned the form
    - Added greeting after login

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("Hello Git!");
+function loginMessage() {
+  let email = document.querySelector("#input-email");
+  alert(`Welcome, ${email.value}! 🎉`);
+}
+
+let loginButton = document.querySelector("#login-button");
+loginButton.addEventListener("click", loginMessage);
